3b3e8b3a818503347f328ead0e2b906ff04297b1
L4rry_Toru4n <larrydennis.ltoruan@gmail.com>
2024-09-30 16:50:23 +0800

    <feat-personel_data: Added a dummy data generation> - Added personel_data variable with tyep of Array of Personel - Added GetPersonels function with return type of Array of Personel <feat-main: Added functions for displaying grid and field data> - Added displayGrid function with a parameter with type of Grid:   + Added title view   + Added symbols for unit type into the grid - Added fieldData function with a parameter with type of Grid:   + Added title view   + Assign full data of personel and coordinates into the field data

--- mini_project_1/core_functions/personel_data.go
+++ mini_project_1/core_functions/personel_data.go
@@ -6,33 +6,42 @@ type Personels map[string]string
 
 var personel_data = [...]Personels{
 	{
-		"name": "Lee 'Apollo' Adama",
-		"rank": "Liutenant",
-		"role": "Viper Pilot",
+		"name":  "Lee Adama",
+		"alias": "Apollo",
+		"rank":  "Liutenant",
+		"role":  "Viper Pilot",
 	},
 	{
-		"name": "Kara 'Starbuck' Thrace",
-		"rank": "Liutenant",
-		"role": "Viper Pilot",
+		"name":  "Kara Thrace",
+		"alias": "Starbuck",
+		"rank":  "Liutenant",
+		"role":  "Viper Pilot",
 	},
 	{
-		"name": "Jay 'Shark' Finnegan",
-		"rank": "Jr. Liutenant",
-		"role": "Raptor Pilot",
+		"name":  "Jay Finnegan",
+		"alias": "Shark",
+		"rank":  "Jr. Liutenant",
+		"role":  "Raptor Pilot",
 	},
 	{
-		"name": "Samuel 'Longshot' Anders",
-		"rank": "Ensign",
-		"role": "Viper Pilot",
+		"name":  "Samuel Anders",
+		"alias": "Longshot",
+		"rank":  "Ensign",
+		"role":  "Viper Pilot",
 	},
 }
 
-func GetPersonels() [MAX]string {
+func GetPersonels() [MAX]Personels {
 
-	var newData [MAX]string
+	var newData [MAX]Personels
 
 	for i := 0; i < len(personel_data); i++ {
-		var temp = personel_data[i]["role"] + " " + personel_data[i]["rank"] + " " + personel_data[i]["name"]
+		var temp = make(map[string]string)
+		temp["role"] = personel_data[i]["role"]
+		temp["rank"] = personel_data[i]["rank"]
+		temp["alias"] = personel_data[i]["alias"]
+		temp["name"] = personel_data[i]["name"]
+
 		newData[i] = temp
 	}
 	return newData
--- mini_project_1/main.go
+++ mini_project_1/main.go
@@ -5,10 +5,90 @@ import (
 	CoreFunctions "golang_basics/mini_project_1/core_functions"
 )
 
+func displayGrid(grid CoreFunctions.Grid) {
+
+	fmt.Printf("\n")
+	fmt.Println("--------------------------------------------------------")
+	fmt.Println("========================| Grid |========================")
+	fmt.Println("--------------------------------------------------------")
+	fmt.Printf("\n")
+
+	fmt.Printf("    ")
+	for i := 0; i < len(grid); i++ {
+		fmt.Printf("  X%d  ", i)
+	}
+	fmt.Println()
+
+	fmt.Printf("     ")
+	for i := 0; i < len(grid)*5; i++ {
+		fmt.Printf("-")
+	}
+
+	fmt.Println()
+	for x := 0; x < len(grid); x++ {
+		fmt.Printf("Y%d", x)
+		fmt.Printf("|")
+		for y := 0; y < len(grid); y++ {
+			if grid[x][y]["value"] != "" {
+				if grid[x][y]["value"] == "Viper Pilot" {
+					fmt.Printf("   V  ")
+				} else if grid[x][y]["value"] == "Raptor Pilot" {
+					fmt.Printf("   R  ")
+				}
+			} else {
+				fmt.Printf("   *  ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func fieldData(grid CoreFunctions.Grid) {
+
+	fmt.Printf("\n")
+	fmt.Println("--------------------------------------------------------")
+	fmt.Println("=====================| Field Data |=====================")
+	fmt.Println("--------------------------------------------------------")
+	fmt.Printf("\n")
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid); j++ {
+			if grid[i][j]["value"] != "" {
+				fmt.Printf("(X%s, Y%s): ", grid[i][j]["coord_x"], grid[i][j]["coord_y"])
+				fmt.Println(grid[i][j]["value"])
+			}
+		}
+	}
+}
+
 func main() {
-	var grid = CoreFunctions.GenerateGrid()
+	var grid_personel_role = CoreFunctions.GenerateGrid()
+	var grid_personel_full = CoreFunctions.GenerateGrid()
 	var personels = CoreFunctions.GetPersonels()
 
-	fmt.Println(grid)
-	fmt.Println(personels)
+	var coord_x = 0
+	var coord_y = 0
+
+	for i := 0; i < len(personels); i++ {
+		if coord_x < 5 && coord_y < 5 {
+			grid_personel_role[coord_x][coord_y]["value"] = personels[i]["role"]
+			coord_x++
+			coord_y++
+		}
+	}
+
+	coord_x = 0
+	coord_y = 0
+
+	for i := 0; i < len(personels); i++ {
+		if coord_x < 4 && coord_y < 4 {
+			grid_personel_full[coord_x][coord_y]["value"] = personels[i]["role"] + ", " + personels[i]["rank"] + ", " + personels[i]["alias"] + ", " + personels[i]["name"]
+			coord_x++
+			coord_y++
+		}
+	}
+
+	displayGrid(grid_personel_role)
+	fieldData(grid_personel_full)
+
 }
